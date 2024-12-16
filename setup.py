c84ad2c7c5fe275502fd2898d176f9e60bcbb5e1
Luciferian Ink <LuciferianInk@protonmail.com>
2024-12-16 01:51:19 -0600

    make installable via pip

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytelatent",
+    version="0.1.0",
+    description="Byte Latent Transformer: Patches Scale Better Than Tokens",
+    author="Meta Platforms, Inc. and affiliates.",
+    url="https://github.com/facebookresearch/blt",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "omegaconf",
+        "msgspec",
+        "rouge-score",
+        "sacrebleu",
+        "sentencepiece",
+        "tiktoken",
+        "fsspec",
+        "blobfile",
+        "wandb",
+        "viztracer",
+        "lm-eval",
+        "scipy",
+        "pynvml",
+        "datatrove",
+        "orjson",
+        "luigi",
+        "pydantic",
+        "altair",
+        "submitit",
+        "typer",
+        "rich"
+    ]
+)
